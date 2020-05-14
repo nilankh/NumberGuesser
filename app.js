eaabffc16efb6ab634a1baacc3fd68088dc76d5f
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-14 15:24:52 +0530

    handle you win codtion added color while giving wrong iput and smallsmall function

--- app.js
+++ app.js
@@ -4,3 +4,52 @@
 // - Notify player of guesses remaining
 // - Notify the player of the correct answer if loose
 // - Let player choose to play again
+
+// Game values
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+// UI elements
+const game = document.querySelector('#game'),
+    minNum = document.querySelector('.min-num'),
+    maxNum = document.querySelector('.max-num'),
+    guessBtn = document.querySelector('#guess-btn'),
+    guessInput = document.querySelector('#guess-input'),
+    message = document.querySelector('.message');
+
+
+// Assign Ui min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+
+// listener for guess
+guessBtn.addEventListener('click', function(){
+    let guess = parseInt(guessInput.value);
+    console.log(guess);
+
+    // Validate
+    if(isNaN(guess) || guess < min || guess > max){
+        setMessage(`Please enter a number betweem ${min} and ${max}`, 'red');
+    }
+// check if won
+    if(guess === winningNum){
+        // Disable input
+        guessInput.disabled = true;
+        // change border color
+        guessInput.style.borderColor = 'green';
+        // Set message
+       setMessage(`${winningNum} is correct, YOU WIN`,'green');
+    } else{
+        
+    }
+});
+
+// set message
+function setMessage(msg, color){
+    message.style.color = color;
+    message.textContent = msg;
+}
+
